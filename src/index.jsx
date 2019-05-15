f2afe8ae3137765ea39145bd46b1b05c431fc74e
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2019-05-16 00:03:01 +0100

    166019796-task(setup): add eslint to codebase
    [Finishes #166019796]

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const Index = () => {
-  return <App />;
-};
+const Index = () => <App />;
 
-ReactDOM.render(<Index />, document.getElementById("app"));
+ReactDOM.render(<Index />, document.getElementById('app'));
