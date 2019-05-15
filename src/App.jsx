e86b3eab8cb112a875b508c083432f435f9aaab3
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2019-05-15 20:22:14 +0100

    165539015-task(setup): setup empty react project
    [Delivers #165539015]

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './App.css';
+
+
+const App = () => (
+  <>
+    <h1 className={style.App}>Questioner Application</h1>
+  </>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const clearBuildPlugin = new CleanWebpackPlugin({
+  dry: true,
+  verbose: true
+});
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: path.join(__dirname, 'public', 'index.html'),
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js)|(jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 2,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              camelCase: true,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|woff|woff2|eot|ttf|svg)$/i,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [htmlPlugin,
+    clearBuildPlugin
+  ],
+  output: {
+    filename: 'index.bundle.js',
+    path: path.resolve(__dirname, 'build')
+  }
+};
