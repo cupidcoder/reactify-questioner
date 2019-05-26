94a6c487ef033751a6eda8721074b686eaff63c9
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2019-05-26 07:39:07 +0100

    165540458-task(ui): build create meetup component
    - build create meetup component
    
    [Finishes #165540458]

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,7 +3,8 @@ import SignUp from './SignUp/SignUp';
 import SignIn from './SignIn/SignIn';
 import Meetups from './Meetups/Meetups';
 import MeetupPage from './MeetupPage/MeetupPage';
+import CreateMeetup from './CreateMeetup/CreateMeetup';
 
 export {
-  LandingPage, SignUp, SignIn, Meetups, MeetupPage
+  LandingPage, SignUp, SignIn, Meetups, MeetupPage, CreateMeetup
 };
